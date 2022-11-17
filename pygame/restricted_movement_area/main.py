3df061bdcaf07cbc3c29a4116cc5729d33244384
LoopedGiraffe <loopedgiraffe@gmail.com>
2022-11-17 12:05:06 +0100

    restricted area movement
    Restricted area and two controll methods: arrows and awsd

---
+++ pygame/restricted_movement_area/main.py
@@ -0,0 +1,61 @@
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#create a display surface
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 300
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Restricted Keyboard Movement")
+
+#SET fps AND CLOCK
+FPS = 60
+clock = pygame.time.Clock()
+
+
+#set game values
+VELOCITY = 5
+
+#load images
+knight_image = pygame.image.load("dragon_center.png")
+knight_rect = knight_image.get_rect()
+knight_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+#the main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #get a list of all keys being pressed down (held)
+    keys = pygame.key.get_pressed()
+    print(keys)
+
+    #move knight continiously
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and knight_rect.left > 0:
+        knight_rect.x -= VELOCITY
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and knight_rect.right < WINDOW_WIDTH:
+        knight_rect.x += VELOCITY
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and knight_rect.top > 0:
+        knight_rect.y -= VELOCITY
+    if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and knight_rect.bottom < WINDOW_HEIGHT:
+        knight_rect.y += VELOCITY
+
+
+    #FILL THE DISPLAY
+    display_surface.fill((0, 0, 0))
+
+    #blit the display
+    display_surface.blit(knight_image, knight_rect)
+
+    #update the display
+    pygame.display.update()
+
+    #thick the clock
+    clock.tick(FPS)
+
+
+#End the game
+pygame.quit()

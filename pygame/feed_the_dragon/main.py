bd24a123ea703b5f1f790ae72664f88393c9b85b
LoopedGiraffe <loopedgiraffe@gmail.com>
2022-11-17 19:02:34 +0100

    Feed the Knight finished game
    Completed game Feed the Knight

--- pygame/feed_the_dragon/main.py
+++ pygame/feed_the_dragon/main.py
@@ -4,8 +4,8 @@ import pygame, random
 pygame.init()
 
 #set display surface
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 600
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed the Knight")
 
@@ -15,9 +15,9 @@ clock = pygame.time.Clock()
 
 
 #set game values
-PLAYER_STARTING_LIVES = 5
+PLAYER_STARTING_LIVES = 10
 PLAYER_VELOCITY = 10
-COIN_STARTING_VELOCITY = 10
+COIN_STARTING_VELOCITY = 5
 COIN_ACCELERATION = .5
 BUFFER_DISTANCE = 100
 
@@ -29,7 +29,7 @@ coin_velocity = COIN_STARTING_VELOCITY
 #set colors
 GREEN = (0, 255, 0)
 DARKGREEN = (10, 50, 10)
-WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 
 
@@ -42,7 +42,7 @@ score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
-title_text = font.render("Feed the Knight", True, GREEN, WHITE)
+title_text = font.render("Feed the Knight", True, YELLOW, DARKGREEN)
 title_rect = title_text.get_rect()
 title_rect.centerx = WINDOW_WIDTH//2
 title_rect.y = 10
@@ -78,12 +78,69 @@ coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
 coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
 
 #main game loop
+#with background music
+pygame.mixer.music.play(-1, 0.0)
 running = True
 while running:
     #check if user wants to quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #check to see if the user wants to move
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_rect.top > 64:
+        player_rect.y -= PLAYER_VELOCITY
+    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
+        player_rect.y += PLAYER_VELOCITY
+
+
+    #MOVE THE COIN
+    if coin_rect.x < 0:
+        #player missed the coin
+        player_lives -= 1
+        miss_sound.play()
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+    else:
+        #move the coin
+        coin_rect.x -= coin_velocity
+    
+    #check for collision
+    if player_rect.colliderect(coin_rect):
+        score += 1
+        coin_sound.play()
+        coin_velocity += COIN_ACCELERATION
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+
+    #UPDATE hud
+    score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+    lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+
+    #CHECK FOR GAME OVER
+    if player_lives == 0:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #pause the game untill player presses a key, then reset the game
+        pygame.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                #the player wants to play again:
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+                    player_rect.y = WINDOW_HEIGHT//2
+                    coin_velocity = COIN_STARTING_VELOCITY
+                    pygame.mixer.music.play(-1, 0.0)
+                    is_paused = False
+                #THE PLAYER WANTS TO QUIT
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
 
     #fill the display
     display_surface.fill(BLACK)
@@ -94,7 +151,7 @@ while running:
     display_surface.blit(lives_text, lives_rect)
 
     #draw white line
-    pygame.draw.line(display_surface, WHITE, (0, 64), (WINDOW_WIDTH, 64), 2)
+    pygame.draw.line(display_surface, YELLOW, (0, 64), (WINDOW_WIDTH, 64), 2)
 
     #bLIT ASSETS TO SCREEN
     display_surface.blit(player_image, player_rect)
